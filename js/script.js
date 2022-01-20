f99e9bb32128f564142a3a9e0e5b5d011947fd2d
Leandro Machado <fahurleandro@gmail.com>
2022-01-20 15:53:33 -0800

    package.json and boilerplate assignment

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/******************************************
+*****************************************/
+
+/*** 
+ * `quotes` array 
+***/
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventL 
