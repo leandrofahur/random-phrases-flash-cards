1658afc5b2196668a99f8145c32bcb0b4c1c9273
Leandro Machado <fahurleandro@gmail.com>
2022-01-20 16:33:13 -0800

    reestructure the css js and index.html file to the public folder

---
+++ public/js/script.js
@@ -0,0 +1,29 @@
+/******************************************
+ *****************************************/
+
+/***
+ * `quotes` array
+ ***/
+const quotes = [
+  {
+    text: "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.",
+    author: "Patrick McKenzie",
+    source: "Twitter",
+    year: "2016",
+  },
+];
+
+/***
+ * `getRandomQuote` function
+ ***/
+
+/***
+ * `printQuote` function
+ ***/
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+ ***/
+
+// document.getElementById("load-quote").addEventListener("click", printQuote);
